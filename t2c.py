46373117d1f216b4b1fc2b54c76fa2905f08bd5f
proprr <therealpropr@gmail.com>
2020-08-01 16:15:44 -0500

    fucktext

--- t2c.py
+++ t2c.py
@@ -9,7 +9,7 @@ res = ""
 output = ""
 
 root.geometry('520x250')
-root.title('Text2Cum')
+root.title('FuckText')
 root.resizable(0,0)
 
 textEntry = Entry(root, textvariable=textContent, width=35)
@@ -20,9 +20,9 @@ charEntry.grid(row=0, column=1)
 
 def writeToFile():
     output = open("output.txt", 'w+')
-    res = textEntry.get().replace(charEntry.get(), "cum")
+    res = textEntry.get().replace(charEntry.get(), "fuck")
     output.write(res)
 
-cumSubmit = Button(root, text="Cum!", command=writeToFile).grid(row=0, column=2)
+fuckSubmit = Button(root, text="Fuck!", command=writeToFile).grid(row=0, column=2)
 
 root.mainloop()
